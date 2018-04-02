b26a615717131292cf70256e7a284f5ec7f90bed
Jorge <barbozajorge@gmail.com>
2018-04-02 16:55:19 -0400

    Styling Weather Page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,24 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function(){
-    // var location = this.props.location;
-    // var temp = this.props.temp;
-
-    var {location, temp} = this.props;
-
-    return(
-      <div>
-        <p>The temperature in {location} is {temp}</p>
-      </div>
-  )}
-});
-
 var WeatherMessage = ({location, temp}) =>{
 
   return(
       <div>
-        <p>The temperature in {location} is {temp}</p>
+        <h3 className="text-center">It's {temp} in {location}</h3>
       </div>
   )
 }
